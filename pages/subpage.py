6757c3a240a308582d87194e8fc2b45860a3489f
Michael Buch <makbuch@Michaels-MBP.fritz.box>
2022-07-11 22:53:50 +0200

    added another page, venv files, as well as container files

--- pages/subpage.py
+++ pages/subpage.py
@@ -3,7 +3,7 @@ from datetime import datetime
 def get_content():
   date_time = datetime.now()
   time_stamp = datetime.utcnow()
-  page_header = '<h1>Home Page @%s</1>' % time_stamp  
-  page_header += '<br><a href="subpage">Sub Page</a>'
+  page_header = '<h1>Sub Page @%s</1>' % time_stamp  
+  page_header += '<br><a href="index">back</a>'
   return page_header
 
