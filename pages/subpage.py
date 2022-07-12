d87b23e2cfbd146e56d229fd539cb4a9078a77b0
Michael Buch <makbuch@Michaels-MBP.fritz.box>
2022-07-12 10:46:28 +0200

    clean upå

--- pages/subpage.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-
-def get_content():
-  date_time = datetime.now()
-  time_stamp = datetime.utcnow()
-  page_header = '<h1>Sub Page @%s</1>' % time_stamp  
-  page_header += '<br><a href="index">back</a>'
-  return page_header
